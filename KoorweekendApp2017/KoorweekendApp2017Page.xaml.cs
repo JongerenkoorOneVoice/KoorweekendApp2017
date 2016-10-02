da2d0d4bf0c45bd337d988fe547b343807469d78
Marc van Nieuwenhuijzen <internet@marcvannieuwenhuijzen.nl>
2016-10-02 23:11:52 +0200

    Leeg Xamarin project

---
+++ KoorweekendApp2017/KoorweekendApp2017Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace KoorweekendApp2017
+{
+	public partial class KoorweekendApp2017Page : ContentPage
+	{
+		public KoorweekendApp2017Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
