7414ef5ae5c33b98069ca05cce5102d819e6dec3
Marc van Nieuwenhuijzen <internet@marcvannieuwenhuijzen.nl>
2016-10-15 23:43:04 +0200

    Added koorweekend basic pages

---
+++ KoorweekendApp2017/Pages/Koorweekend2017/Koorweekend2017Page.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace KoorweekendApp2017.Pages.Koorweekend2017
+{
+	public partial class Koorweekend2017Page : ContentPage
+	{
+		public Koorweekend2017Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ KoorweekendApp2017/Pages/Koorweekend2017/Koorweekend2017Spel2Page.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace KoorweekendApp2017.Pages.Koorweekend2017
+{
+	public partial class Koorweekend2017Spel2Page : ContentPage
+	{
+		public Koorweekend2017Spel2Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
