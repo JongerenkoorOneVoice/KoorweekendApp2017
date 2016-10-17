36729180387f011c967a84daee4b62761027bcde
Marc van Nieuwenhuijzen <internet@marcvannieuwenhuijzen.nl>
2016-10-17 23:44:06 +0200

    Repertoire werkend gemaakt

--- KoorweekendApp2017/Pages/RepertoirePage.xaml.cs
+++ KoorweekendApp2017/Pages/RepertoirePage.xaml.cs
@@ -13,9 +13,34 @@ namespace KoorweekendApp2017.Pages
     public partial class RepertoirePage : ContentPage
     {
         public RepertoirePage()
-        {
-            InitializeComponent();
-            List<Song> songs = RestHelper.GetRestDataFromUrl<Song>("http://www.jongerenkooronevoice.nl/songs/all").Result;
+        {           
+			try
+			{
+				InitializeComponent();
+				songListView.ItemSelected += OnSongSelected;
+				List<Song> songs = RestHelper.GetRestDataFromUrl<Song>("http://www.jongerenkooronevoice.nl/songs/all").Result;
+				songs = songs.OrderBy(song => song.Title).ToList();
+				songListView.ItemsSource = songs;
+
+
+			}
+			catch (Exception ex)
+			{
+				var a = ex.Message;
+			}
         }
+
+		void OnSongSelected(object sender, SelectedItemChangedEventArgs e)
+		{
+			var item = e.SelectedItem as Song;
+			if (item != null)
+			{
+				Navigation.PushAsync(new SongSinglePage() { BindingContext = item });
+				//Detail = new MainNavigationPage((Page)Activator.CreateInstance(typeof(ContactSinglePage)));
+				songListView.SelectedItem = null;
+
+				//IsPresented = false;
+			}
+		}
     }
 }
