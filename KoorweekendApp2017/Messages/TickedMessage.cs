ee11f57a48904890485a02cd0680ac724d8dfce7
marcvannieuwenhuijzen <internet@marcvannieuwenhuijzen.nl>
2016-10-22 20:12:40 +0200

    Poging twee

---
+++ KoorweekendApp2017/Messages/TickedMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace KoorweekendApp2017.Messages
+{
+	public class TickedMessage { 
+	
+		public string Message { get; set; }
+	}
+}
---
+++ iOS/Tasks/ApiContactSyncTask.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading;
+using UIKit;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using KoorweekendApp2017.Messages;
+
+
+namespace KoorweekendApp2017.iOS.Tasks{
+                            
+	public class ApiContactSyncTask
+	{
+		nint _taskId;
+		CancellationTokenSource _cts;
+
+		public ApiContactSyncTask()
+		{
+
+		}
+
+		public async Task Start()
+		{
+			_cts = new CancellationTokenSource();
+
+			_taskId = UIApplication.SharedApplication.BeginBackgroundTask("ApiContactSync", OnExpiration);
+
+			try
+			{
+
+				KoorweekendApp2017.Tasks.DataSync.UpdateContactsInDbFromApi();
+
+			}
+			catch (OperationCanceledException)
+			{
+			}
+			finally
+			{
+				if (_cts.IsCancellationRequested)
+				{
+					var message = new StopApiContactSyncMessage();
+					Device.BeginInvokeOnMainThread(
+						() => MessagingCenter.Send(message, "CancelledMessage")
+					);
+				}
+			}
+
+			UIApplication.SharedApplication.EndBackgroundTask(_taskId);
+		}
+
+		public void Stop()
+		{
+			_cts.Cancel();
+		}
+
+		void OnExpiration()
+		{
+			_cts.Cancel();
+		}
+	}
+}
