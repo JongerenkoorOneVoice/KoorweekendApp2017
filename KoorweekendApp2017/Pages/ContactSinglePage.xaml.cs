61672b88dd34a0709ec77638b9547456347fa6ff
CoDaR96 <CoDaR96@DESKTOP-0CA8M62>
2016-11-20 06:50:52 -0800

    fixes, Android menupage, google maps navigation in events kleding foto toegevoegd

--- KoorweekendApp2017/Pages/ContactSinglePage.xaml.cs
+++ KoorweekendApp2017/Pages/ContactSinglePage.xaml.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using KoorweekendApp2017.Models;
 using Xamarin.Forms;
 
 namespace KoorweekendApp2017.Pages
 {
-	public partial class ContactSinglePage : ContentPage
-	{
-		public ContactSinglePage()
-		{
+    public partial class ContactSinglePage : ContentPage
+    {
+        public ContactSinglePage()
+        {
 
             InitializeComponent();
             /*
@@ -33,9 +34,9 @@ namespace KoorweekendApp2017.Pages
         {
             base.OnAppearing();
 
-            if (Convert.ToString(Adres.Text).ToLower() != "geen")
+            if (Convert.ToString(Adres.Text).ToLower() != "geen" && Convert.ToString(Adres.Text).ToLower() != "x" && Convert.ToString(Adres.Text).ToLower() != "onbekend")
             {
-                Adres.TextColor = Color.FromHex("#0645AD");
+                Adres.TextColor = Color.FromHex("#FF0000");//#0645AD voor blauw
                 var adresOpen = new TapGestureRecognizer();
                 adresOpen.Tapped += (s, e) => {
                     Device.OpenUri(new Uri(string.Format("https://www.google.nl/maps/place/{0},{1}", Adres.Text.Replace(' ', '+'), Plaats.Text)));
@@ -43,9 +44,9 @@ namespace KoorweekendApp2017.Pages
                 Adres.GestureRecognizers.Add(adresOpen);
             }
 
-            if (Convert.ToString(Email.Text).ToLower() != "geen")
+            if (Convert.ToString(Email.Text).ToLower() != "geen" && Convert.ToString(Email.Text).ToLower() != "x" && Convert.ToString(Email.Text).ToLower() != "onbekend")
             {
-                Email.TextColor = Color.FromHex("#0645AD");
+                Email.TextColor = Color.FromHex("#FF0000");
                 var emailOpen = new TapGestureRecognizer();
                 emailOpen.Tapped += (s, e) => {
                     Device.OpenUri(new Uri(string.Format("mailto:{0}", Email.Text)));
@@ -53,24 +54,24 @@ namespace KoorweekendApp2017.Pages
                 Email.GestureRecognizers.Add(emailOpen);
             }
 
-            if (Convert.ToString(Mobiel.Text).ToLower() != "geen")
+            if (Convert.ToString(Mobiel.Text).ToLower() != "geen" && Convert.ToString(Mobiel.Text).ToLower() != "x" && Convert.ToString(Mobiel.Text).ToLower() != "onbekend")
             {
-                Mobiel.TextColor = Color.FromHex("#0645AD");
+                Mobiel.TextColor = Color.FromHex("#FF0000");
                 var mobileOpen = new TapGestureRecognizer();
                 mobileOpen.Tapped += (s, e) =>
                 {
-                        Device.OpenUri(new Uri(string.Format("tel:{0}", Mobiel.Text)));
+                    Device.OpenUri(new Uri(string.Format("tel:{0}", Mobiel.Text)));
                 };
                 Mobiel.GestureRecognizers.Add(mobileOpen);
             }
 
-            if (Convert.ToString(Telefoon.Text).ToLower() != "geen")
+            if (Convert.ToString(Telefoon.Text).ToLower() != "geen" && Convert.ToString(Telefoon.Text).ToLower() != "x" && Convert.ToString(Telefoon.Text).ToLower() != "onbekend")
             {
-                Telefoon.TextColor = Color.FromHex("#0645AD");
+                Telefoon.TextColor = Color.FromHex("#FF0000");
                 var phoneOpen = new TapGestureRecognizer();
                 phoneOpen.Tapped += (s, e) =>
                 {
-                        Device.OpenUri(new Uri(string.Format("tel:{0}", Telefoon.Text)));
+                    Device.OpenUri(new Uri(string.Format("tel:{0}", Telefoon.Text)));
                 };
                 Telefoon.GestureRecognizers.Add(phoneOpen);
             }
--- KoorweekendApp2017/Pages/EventSinglePage.xaml.cs
+++ KoorweekendApp2017/Pages/EventSinglePage.xaml.cs
@@ -6,62 +6,76 @@ using Xamarin.Forms;
 
 namespace KoorweekendApp2017.Pages
 {
-	public partial class EventSinglePage : ContentPage
-	{
+    public partial class EventSinglePage : ContentPage
+    {
 
-		public EventSinglePage()
-		{
-			InitializeComponent();
-			songForEventListView.ItemSelected += OnSongSelected;
-		}
+        public EventSinglePage()
+        {
+            InitializeComponent();
+            songForEventListView.ItemSelected += OnSongSelected;
+        }
 
-		protected override void OnBindingContextChanged()
-		{
-			base.OnBindingContextChanged();
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (Convert.ToString(Adres.Text).ToLower() != "geen" && Convert.ToString(Adres.Text).ToLower() != "x" && Convert.ToString(Adres.Text).ToLower() != "onbekend")
+            {
+                Adres.TextColor = Color.FromHex("#FF0000"); //#0645AD voor blauw
+                var adresOpen = new TapGestureRecognizer();
+                adresOpen.Tapped += (s, e) => {
+                    Device.OpenUri(new Uri(string.Format("https://www.google.nl/maps/place/{0},{1}", Adres.Text.Replace(' ', '+'), Plaats.Text)));
+                };
+                Adres.GestureRecognizers.Add(adresOpen);
+            }
+        }
 
-			var currentEvent = BindingContext as Event;
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
 
-			List<Song> songsForEvent = new List<Song>();
-			if (currentEvent.Songs != null)
-			{
-				foreach (int songId in currentEvent.Songs)
-				{
-					// Check if the song is still in the database
-					Song songForEvent = App.Database.Songs.GetById(songId);
-					if (songForEvent == null)
-					{
-						// Try to update the data from the webservicev
-						DataSync.UpdateSongsInDbFromApi(true);
-						songForEvent = App.Database.Songs.GetById(songId);
-						if (songForEvent == null)
-						{
-							songForEvent = new Song()
-							{
-								Id = 0,
-								Title = "Dit liedje is verwijderd",
-								Lyrics = "Het liedje dat je zoekt staat niet meer in de app en je kan het daarom niet meer bekijken\r\n\nDenk je dat dit niet kopt? Probeer eerst om de app te sluiten en opnieuw te starten met internet aan.\r\nNeem als het dan nog niet werkt even contact op met het bestuur.",
-								LastModified = DateTime.Now
-						};
-						}
-					}
-					songsForEvent.Add(songForEvent);
-				}
-				songsHeaderTitle.IsVisible = songsForEvent.Count > 0;
-				currentEvent.SongItems = songsForEvent;
-			}
+            var currentEvent = BindingContext as Event;
 
-			songForEventListView.ItemsSource = currentEvent.SongItems;
-		}
+            List<Song> songsForEvent = new List<Song>();
+            if (currentEvent.Songs != null)
+            {
+                foreach (int songId in currentEvent.Songs)
+                {
+                    // Check if the song is still in the database
+                    Song songForEvent = App.Database.Songs.GetById(songId);
+                    if (songForEvent == null)
+                    {
+                        // Try to update the data from the webservicev
+                        DataSync.UpdateSongsInDbFromApi(true);
+                        songForEvent = App.Database.Songs.GetById(songId);
+                        if (songForEvent == null)
+                        {
+                            songForEvent = new Song()
+                            {
+                                Id = 0,
+                                Title = "Dit liedje is verwijderd",
+                                Lyrics = "Het liedje dat je zoekt staat niet meer in de app en je kan het daarom niet meer bekijken\r\n\nDenk je dat dit niet kopt? Probeer eerst om de app te sluiten en opnieuw te starten met internet aan.\r\nNeem als het dan nog niet werkt even contact op met het bestuur.",
+                                LastModified = DateTime.Now
+                            };
+                        }
+                    }
+                    songsForEvent.Add(songForEvent);
+                }
+                songsHeaderTitle.IsVisible = songsForEvent.Count > 0;
+                currentEvent.SongItems = songsForEvent;
+            }
 
-		void OnSongSelected(object sender, SelectedItemChangedEventArgs e)
-		{
-			var item = e.SelectedItem as Song;
-			if (item != null)
-			{
-				Navigation.PushAsync(new SongSinglePage() { BindingContext = item });
-				songForEventListView.SelectedItem = null;
-			}
-		}
+            songForEventListView.ItemsSource = currentEvent.SongItems;
+        }
 
-	}
+        void OnSongSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as Song;
+            if (item != null)
+            {
+                Navigation.PushAsync(new SongSinglePage() { BindingContext = item });
+                songForEventListView.SelectedItem = null;
+            }
+        }
+
+    }
 }
