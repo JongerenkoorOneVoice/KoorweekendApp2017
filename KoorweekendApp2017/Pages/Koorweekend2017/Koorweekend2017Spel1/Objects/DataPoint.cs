41f23f40b8f8f428808ba79b0d85e1738d9dcabf
Marc <info@webvakman.nl>
2017-03-25 12:21:39 +0100

    Changes for game 1

---
+++ KoorweekendApp2017/Pages/Koorweekend2017/Koorweekend2017Spel1/Objects/DataPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using CocosSharp;
+
+namespace KoorweekendApp2017.Koorweekend2017Spel1.Objects
+{
+	public class DataLayer : CCLayer
+	{
+		public CCNode RotatingDataLayer { get; set; } = new CCNode();
+
+		public List<CCNode> DataPoints { get; set; } = new List<CCNode>();
+
+		public DataLayer()
+		{
+			
+
+			RotatingDataLayer.AnchorPoint = CCPoint.AnchorMiddle;
+			RotatingDataLayer.Position = new CCPoint(500f, 750f);
+			RotatingDataLayer.ContentSize = new CCSize(1000f, 1000f);
+			RotatingDataLayer.Color = CCColor3B.Blue;
+			AddChild(RotatingDataLayer);
+
+
+		}
+
+
+	}
+}
