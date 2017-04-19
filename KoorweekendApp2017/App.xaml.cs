5d626095605f5596c4cd66aca34460c592a41f05
Marc <info@webvakman.nl>
2017-04-20 00:01:23 +0200

    Versie 1.2.0.0 RC

--- KoorweekendApp2017/App.xaml.cs
+++ KoorweekendApp2017/App.xaml.cs
@@ -38,7 +38,6 @@ namespace KoorweekendApp2017
 			AppWebService = new AppWebService();
 			Network = CrossConnectivity.Current;
 			SettingsScreenUri = uri;
-
 			if (GlobalSettingsHelper.VersionBiggerThanAppVersion(GlobalSettings.MinimumVersionToAllowRunning))
 			{
 				// To be sure data is correct, try to update data from web api and check again
